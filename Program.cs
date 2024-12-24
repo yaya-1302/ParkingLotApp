4b7d53a13e9815132c73013e2cb16dba35dbda84
Tiara Oktavian <tiaraoktaviani40@gmail.com>
2024-12-24 14:06:31 +0700

    feat: main class

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ParkingLotApp.Controller;
+using ParkingLotApp.Services;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		ParkingManager manager = ParkingManagerController.CreateParkingManager();
+
+		// ParkingManager? manager = null;
+
+		do
+		{
+			Console.WriteLine("Enter command:");
+			var command = Console.ReadLine();
+			var commandArgs = command?.Split(' ');
+
+			CommandHandler.HandleCommand(commandArgs, manager);
+		} while (true);
+	}
+}
