3489cab3eb29532828311f5d3cf50bd31afaa91d
Tiara Oktavian <tiaraoktaviani40@gmail.com>
2024-12-24 18:43:32 +0700

    fix: fix typo

--- Program.cs
+++ Program.cs
@@ -17,10 +17,10 @@ class Program
 		);
 		Console.WriteLine("2. Leave your vehicle:\n#leave [parking slot number]\n");
 		Console.WriteLine("3. See parking:\n#status\n");
-		Console.WriteLine("4. Count type behicle:\n#type_of_vehicles [Mobil/Motor]\n");
-		Console.WriteLine("5. Find vehicle with odd plate:\n#registration_numbers_for_vehicles_with_odd_plate\n");
-		Console.WriteLine("6. Find vehicle with even plate:\n#registration_numbers_for_vehicles_with_even_plate\n");
-		Console.WriteLine("7. Find vehicle by color:\n#registration_numbers_for_vehicles_with_color\n");
+		Console.WriteLine("4. Count type vehicle:\n#type_of_vehicles [Mobil/Motor]\n");
+		Console.WriteLine("5. Find vehicles with odd plate:\n#registration_numbers_for_vehicles_with_odd_plate\n");
+		Console.WriteLine("6. Find vehicles with even plate:\n#registration_numbers_for_vehicles_with_even_plate\n");
+		Console.WriteLine("7. Find vehicles by color:\n#registration_numbers_for_vehicles_with_color\n");
 		Console.WriteLine("8. Find slot parking by color:\n#slot_numbers_for_vehicles_with_color\n");
 		Console.WriteLine("9. Find slot parking by plate:\n#slot_number_for_registration_number\n");
 		Console.WriteLine("10. To exit:\n#exit\n");
